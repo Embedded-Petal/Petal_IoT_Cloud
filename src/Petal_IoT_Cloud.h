1431411724450a323000892b65861f5ea4b6479a
Embedded Team <petalsautomationembedded@gmail.com>
2026-03-05 11:12:20 +0530

    Update Petal_IoT_Cloud.h

--- src/Petal_IoT_Cloud.h
+++ src/Petal_IoT_Cloud.h
@@ -1,3 +1,14 @@
+#ifndef PETAL_IOT_CLOUD_H
+#define PETAL_IOT_CLOUD_H
+
 #include "iotCloud.h"
+
+#ifdef PETAL_LED_MATRIX
 #include "PetalLedmatrix.h"
+#endif
+
+#ifdef PETAL_STEPPER_MOTOR
 #include "StepperMotor.h"
+#endif
+
+#endif
